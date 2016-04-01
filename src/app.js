c49eed121ec2f4a38b92289f9ea5fabda6d602b9
Nathan Winch <nwinch@gmail.com>
2016-04-01 14:38:48 +1100

    Move index.js to /src/app.sj

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const React = require('react');
 const _ = require('lodash');
 const DOM = require('react-dom');
-const config = require('./config');
+const config = require('../config');
 
-const List = React.createFactory(require('./lib/list.js'));
-const Table = React.createFactory(require('./lib/table.js'));
-const Creator = React.createFactory(require('./lib/creator.js'));
+const List = React.createFactory(require('../lib/list.js'));
+const Table = React.createFactory(require('../lib/table.js'));
+const Creator = React.createFactory(require('../lib/creator.js'));
 
 if (!config.apiUrl) {
   if (localStorage.apiUrl) config.apiUrl = localStorage.apiUrl;
