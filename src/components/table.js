4213008b73e4fd84f1dde0084fad42d11febd765
Simon Taylor <nizmox@gmail.com>
2016-04-07 10:59:37 +1000

    remove unused code from table

--- src/components/table.js
+++ src/components/table.js
@@ -26,22 +26,6 @@ module.exports = React.createClass({
     };
   },
 
-  getInitialState: function() {
-    return {
-      relationships: this.calcRelations(this.props.schema)
-    };
-  },
-
-  calcRelations: function(schema) {
-    if (!schema.links) return {};
-    return schema.links.reduce((r, link) => {
-      if (schema.properties[link.rel]) {
-        r[link.rel] = link.href;
-      }
-      return r;
-    }, {});
-  },
-
   getHeaderRow: function(headers) {
     const columns = headers.map((header, index) => {
       return TableHeaderColumn({key: index}, header);
@@ -65,13 +49,10 @@ module.exports = React.createClass({
     return React.DOM.div(
       null,
       React.DOM.h3({key: 'header'}, this.props.name),
-      this.state.instance ?
-        React.DOM.dialog({open: true, key: 'instance', id: 'instance'}, this.state.instance)
-        : null, // TODO allow a specifed element or DOM node to be passed in at instantiation
-      React.DOM.table({key: 'table'}, [
+      React.DOM.table({key: 'table'},
         React.DOM.thead({key: 'thead'}, this.getHeaderRow(this.props.headers)),
         React.DOM.tbody({key: 'tbody'}, this.getRows())
-      ])
+      )
     );
   }
 });
