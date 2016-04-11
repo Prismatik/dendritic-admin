7bfd360d88b9a52ce0a8c38dffb72943ac4a7a44
Nathan Winch <nwinch@gmail.com>
2016-04-12 09:57:38 +1000

    Move components to main

--- src/app.js
+++ src/app.js
@@ -1,17 +1,12 @@
 const React = require('react');
 const _ = require('lodash');
 const DOM = require('react-dom');
-const extractHeaders = require('./lib/transformers/schema').extractHeaders;
 const config = require('../config');
 const css = require('./css/style.css');
 
-import listener from './components/listener';
-import table from './components/table';
+import main from './components/main';
 
-const List = React.createFactory(require('./components/list.js'));
-const Table = React.createFactory(table);
-const Creator = React.createFactory(require('./components/creator.js'));
-const ListeningTable = React.createFactory(listener(Table));
+const Main = React.createFactory(main);
 
 if (!config.apiUrl) {
   if (localStorage.apiUrl) config.apiUrl = localStorage.apiUrl;
@@ -23,22 +18,11 @@ const store = {
   dispatch: action => {
     store.state = _.merge(store.state, action.payload);
     const state = store.state;
-    console.log('state is', state);
-    DOM.render(List({ store: store }), document.getElementById('list'));
 
-    if (state.selectedModel) {
-      const schema = state.schema[state.selectedModel];
-      const opts = {
-        schema: schema,
-        name: state.selectedModel,
-        pluralName: schema.pluralName,
-        apiUrl: localStorage.apiUrl
-      };
-      DOM.render(Creator(opts), document.getElementById('creator'));
-
-      const tableProps = getTableProps(schema, localStorage.apiUrl);
-      DOM.render(ListeningTable(tableProps), document.getElementById('table'));
-    }
+    DOM.render(
+      Main({ store, apiUrl: localStorage.apiUrl }),
+      document.getElementById('app')
+    );
   },
   state: {
     endpoints: []
@@ -56,12 +40,3 @@ fetch(config.apiUrl+'/schema')
   const endpoints = Object.keys(json).map(k => { return { name: k };});
   store.dispatch({ payload: { schema: json, endpoints: endpoints }});
 });
-
-function getTableProps(schema, apiUrl) {
-  return {
-    schema: schema,
-    name: schema.pluralName,
-    headers: extractHeaders(schema.properties),
-    host: apiUrl + '/' + schema.pluralName
-  };
-}
