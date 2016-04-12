feecff1162996f499f6b70812a42ef8b5e8d1ee1
Nathan Winch <nwinch@gmail.com>
2016-04-12 15:52:50 +1000

    Refactor to kick off with store

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
-const React = require('react');
-const _ = require('lodash');
-const DOM = require('react-dom');
-const config = require('../config');
-const css = require('./css/style.css');
-
-import main from './components/main';
-
-const Main = React.createFactory(main);
+import React, { createElement } from 'react';
+import DOM from 'react-dom';
+import { Provider } from 'react-redux';
+import main from './containers/main';
+import css from './css/style.css';
+import configureStore, { reducer } from './redux/store';
+import config from '../config';
 
 if (!config.apiUrl) {
   if (localStorage.apiUrl) config.apiUrl = localStorage.apiUrl;
@@ -14,29 +12,9 @@ if (!config.apiUrl) {
   localStorage.apiUrl = config.apiUrl;
 }
 
-const store = {
-  dispatch: action => {
-    store.state = _.merge(store.state, action.payload);
-    const state = store.state;
+const Main = React.createFactory(main);
+const store = configureStore(reducer, { api: { url: localStorage.apiUrl } });
 
-    DOM.render(
-      Main({ store, apiUrl: localStorage.apiUrl }),
-      document.getElementById('app')
-    );
-  },
-  state: {
-    endpoints: []
-  }
-};
-
-const render = state => {
-};
-
-store.dispatch({});
-
-fetch(config.apiUrl+'/schema')
-.then(res => res.json())
-.then(json => {
-  const endpoints = Object.keys(json).map(k => { return { name: k };});
-  store.dispatch({ payload: { schema: json, endpoints: endpoints }});
-});
+DOM.render((
+  createElement(Provider, { store }, Main())
+), document.getElementById('app'));
