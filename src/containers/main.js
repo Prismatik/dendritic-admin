343a009aa85ec935133a684042bc2f9b96a203d3
Nathan Winch <nwinch@gmail.com>
2016-04-13 09:10:03 +1000

    Refactor main to container

--- src/containers/main.js
+++ src/containers/main.js
@@ -1,54 +1,60 @@
-import { flowRight } from 'lodash';
+import { find, flow, map, toArray } from 'lodash';
 import React, { Component, createFactory, DOM, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { resolve } from 'react-resolver';
 import { bindActionCreators } from 'redux';
-import { extractHeaders } from '../lib/transformers/schema';
-import { getApiSuccess } from '../redux/actions/api';
-import { getCollectionsSuccess } from '../redux/actions/collections';
-import * as navActions from '../redux/actions/nav';
 import creator from '../components/creator';
-import listener from '../components/listener';
 import nav from '../components/nav';
+import listener from '../components/socket_listener';
 import table from '../components/table';
+import { getApiSuccess } from '../redux/actions/api';
+import {
+  getCollectionsSuccess,
+  updateCollection
+} from '../redux/actions/collections';
+import * as navActions from '../redux/actions/nav';
+import { extractHeaders } from '../lib/transformers/schema';
 
 const Creator = createFactory(creator);
 const Nav = createFactory(nav);
 const Table = createFactory(table);
-const ListeningTable = createFactory(listener(Table));
 
 export class Main extends Component {
   render() {
     const {
       api: { url, schema },
-      collections,
       nav: { active },
+      collections,
       navActions
     } = this.props;
+
+    const activeSchema = schema[active];
 
     return DOM.div({ className: 'container row' },
       DOM.div({ className: 'col s2' },
         Nav({
           items: Object.keys(collections),
-          itemOnClick: (e, name) => {
+          itemOnClick: (e, item) => {
             e.preventDefault();
-            navActions.setNavActive(name);
+
+            const found = find(schema, i => i.pluralName == item);
+            navActions.setNavActive(found.name);
           }
         })
       ),
-      DOM.div({ className: 'col' },
-        active
-          ? DOM.div(null, Creator({
-                schema: schema[active],
+      DOM.div({ className: 'col s8' },
+        activeSchema
+          ? DOM.div(null,
+              Creator({
+                schema: activeSchema,
                 name: active,
-                pluralName: schema[active].pluralName,
-                apiUrl: url
+                apiUrl: url,
+                pluralName: activeSchema.pluralName
               }),
-              ListeningTable({
-                schema: schema[active],
-                name: schema[active].pluralName,
-                headers: extractHeaders(schema[active].properties),
-                host: url + '/' + schema[active].pluralName
+              Table({
+                name: activeSchema.pluralName,
+                headers: extractHeaders(activeSchema.properties),
+                data: toArray(collections[activeSchema.pluralName])
               })
             )
           : null
@@ -59,19 +65,9 @@ export class Main extends Component {
 
 Main.propTypes = {
   api: PropTypes.object.isRequired,
-  collections: PropTypes.object.isRequired
+  collections: PropTypes.object.isRequired,
+  nav: PropTypes.object.isRequired
 };
-
-const Resolved = resolve('init', props => {
-  if (props.api.schema) return;
-
-  fetch(props.api.url + '/schema')
-    .then(res => res.json())
-    .then(json => {
-      props.dispatch(getApiSuccess(json));
-      props.dispatch(getCollectionsSuccess(Object.keys(json)));
-    });
-});
 
 const Connected = connect(state => ({
   api: state.api,
@@ -82,4 +78,25 @@ const Connected = connect(state => ({
   dispatch
 }));
 
-export default flowRight(Connected, Resolved)(Main);
+const Resolved = resolve('init', ({ api, dispatch }) => {
+  if (api.schema) return;
+
+  return fetch(api.url + '/schema')
+    .then(res => res.json())
+    .then(json => {
+      dispatch(getApiSuccess(json));
+      dispatch(getCollectionsSuccess(map(json, 'pluralName')));
+    });
+});
+
+const SocketListener = listener(({ api, collections, dispatch }) => {
+  return Object.keys(collections).map(collection => ({
+    host: api.url + '/' + collection,
+    event: 'record',
+    handler: data => {
+      dispatch(updateCollection({ collection, item: data.new_val }));
+    }
+  }));
+});
+
+export default flow(SocketListener, Resolved, Connected)(Main);
