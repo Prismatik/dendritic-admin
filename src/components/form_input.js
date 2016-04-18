b4c07312420612d2f018ddf355a27b335fd1aa10
Nathan Winch <nwinch@gmail.com>
2016-04-18 10:51:01 +1000

    Semicolon

--- src/components/form_input.js
+++ src/components/form_input.js
@@ -3,5 +3,5 @@ import React, { DOM } from 'react';
 export default function FormInput(props) {
   return DOM.div({ className: 'input-field' },
     DOM.input({ ...props, placeholder: props.name })
-  )
+  );
 };
