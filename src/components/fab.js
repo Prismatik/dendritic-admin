394a4c1b24ae8b780a50c8dbbcfc442294465f22
Nikolay Nemshilov <nemshilov@gmail.com>
2016-04-19 14:22:00 +1000

    #35 connecting the delete button to a redux delete item action

--- src/components/fab.js
+++ src/components/fab.js
@@ -1,7 +1,7 @@
 import React, { DOM } from 'react';
 
-const Fab = ({ color, children }) => {
-  return DOM.a({ href: '#', className: `btn-floating waves-effect waves-light ${color}`},
+const Fab = ({ color, title, onClick, children }) => {
+  return DOM.a({ href: '#', className: `btn-floating waves-effect waves-light ${color}`, title, onClick},
     DOM.i({ className: 'material-icons' }, children)
   );
 };
--- src/containers/collection.js
+++ src/containers/collection.js
@@ -1,10 +1,11 @@
 import { toArray } from 'lodash';
 import React, { Component, createFactory, createElement, DOM } from 'react';
 import { connect } from 'react-redux';
+import { removeFromCollection } from '../redux/actions/collections';
 import creator from '../components/creator';
 import table from '../components/table';
-import { extractHeaders } from '../lib/transformers/schema';
 import Fab from '../components/fab';
+import { extractHeaders } from '../lib/transformers/schema';
 
 const Creator = createFactory(creator);
 const Table = createFactory(table);
@@ -33,9 +34,16 @@ export class Collection extends Component {
 
   actionsFor(entry) {
     return DOM.div({ className: 'actions' },
-      createElement(Fab, { color: 'green' }, 'mode_edit'),
-      createElement(Fab, { color: 'red' }, 'delete')
+      createElement(Fab, { color: 'green', title: 'Edit the record' }, 'mode_edit'),
+      createElement(Fab, { color: 'red', title: 'Delete the record', onClick: e => this.removeItem(e, entry) }, 'delete')
     );
+  }
+
+  removeItem(event, entry) {
+    const { schema, removeFromCollection } = this.props;
+
+    event.preventDefault();
+    removeFromCollection(entry, schema.name);
   }
 }
 
@@ -43,6 +51,10 @@ const Connected = connect((state, props) => ({
   schema: state.api.schema[props.params.name],
   apiUrl: state.api.url,
   collection: state.collections[props.params.name]
+}), dispatch => ({
+  removeFromCollection: (item, collectionName) => {
+    dispatch(removeFromCollection({ item, collection: collectionName }));
+  }
 }));
 
 export default Connected(Collection);
