2e11e1cd5cc76d32cc8ddd08e7c3281d4904b932
Nathan Winch <nwinch@gmail.com>
2016-04-20 15:00:58 +1000

    Freeze schema fixture

--- test/fixtures/valid_schema.js
+++ test/fixtures/valid_schema.js
@@ -1,4 +1,5 @@
-import { cloneDeep } from 'lodash';
+import { flow, cloneDeep } from 'lodash';
+import { deepFreeze } from 'root/src/lib/object';
 import schema from './valid_schema.json';
 
-export const ValidSchema = cloneDeep(schema);
+export const ValidSchema = flow(cloneDeep, deepFreeze)(schema);
