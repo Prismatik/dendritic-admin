2ec8b4669930f375a51d2a16ddb917f3f5084fa0
Nathan Winch <nwinch@gmail.com>
2016-04-27 14:55:54 +1000

    Make url a const

--- test/fixtures/valid_state.js
+++ test/fixtures/valid_state.js
@@ -1,10 +1,12 @@
 import { fromPairs, map } from 'lodash';
 import { ValidSchema } from './valid_schema';
 
+const API_URL = 'http://testing.com';
+
 export default function ValidState() {
   return {
     api: {
-      url: 'http://testing.com',
+      url: API_URL,
       schema: ValidSchema
     },
     collections: fromPairs(map(Object.keys(ValidSchema), i => [i, {}]))
