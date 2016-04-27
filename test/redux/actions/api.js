66bb593100ae3f7a6b4bbd255253e401740d4722
Nathan Winch <nwinch@gmail.com>
2016-04-27 15:59:26 +1000

    Test api actions

---
+++ test/redux/actions/api.js
@@ -0,0 +1,17 @@
+import * as actions from 'root/src/redux/actions/api';
+
+describe('./redux/actions/api', function() {
+  describe('.getApiSuccess', function() {
+    it('must return type GET_API_SUCCESS', function() {
+      const action = actions.getApiSuccess({});
+      action.type.must.eql('GET_API_SUCCESS');
+    });
+  });
+
+  describe('.updateApiChangefeedState', function() {
+    it('must return type UPDATE_API_CHANGEFEED_STATE', function() {
+      const action = actions.updateApiChangefeedState({});
+      action.type.must.eql('UPDATE_API_CHANGEFEED_STATE');
+    });
+  });
+});
