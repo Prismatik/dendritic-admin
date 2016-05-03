31de34ed8dc438128e0b9bf58276207c89617681
Nathan Winch <nwinch@gmail.com>
2016-05-03 15:56:28 +1000

    Refactor + add tests

--- src/components/table.js
+++ src/components/table.js
@@ -1,46 +1,37 @@
 import { values } from 'lodash';
-import React, {
-  Component,
-  createFactory,
-  DOM,
-  PropTypes
-} from 'react';
-import tc from './table_column';
-import thc from './table_header_column';
-import tr from './table_row';
-
-const TableColumn = createFactory(tc);
-const TableHeaderColumn = createFactory(thc);
-const TableRow = createFactory(tr);
+import React, { Component, createElement, DOM, PropTypes } from 'react';
+import TableColumn from './table_column';
+import TableHeaderColumn from './table_header_column';
+import TableRow from './table_row';
 
 export default class Table extends Component {
-  getHeaderRow(headers) {
-    const columns = headers.map((header, index) => {
-      return TableHeaderColumn({ key: index }, header);
-    });
-    return TableRow({ columns });
-  }
-
-  getRows(data) {
-    return data.map((row, index) => {
-      const columns = values(row).map((column, index) => {
-        return TableColumn({ key: index }, column);
-      });
-
-      return TableRow({ key: index, columns });
-    });
-  }
-
   render() {
     const { name, headers, data } = this.props;
 
     return DOM.div(null,
       DOM.h3(null, name),
       DOM.table({ className: 'highlight' },
-        DOM.thead(null, this.getHeaderRow(headers)),
-        DOM.tbody(null, this.getRows(data))
+        DOM.thead(null, this._getHeaderRow(headers)),
+        DOM.tbody(null, this._getRows(data))
       )
     );
+  }
+
+  _getHeaderRow(headers) {
+    const columns = headers.map((header, index) => {
+      return createElement(TableHeaderColumn, { key: index }, header);
+    });
+    return createElement(TableRow, { columns });
+  }
+
+  _getRows(data) {
+    return data.map((row, index) => {
+      const columns = values(row).map((column, index) => {
+        return createElement(TableColumn, { key: index }, column);
+      });
+
+      return createElement(TableRow, { key: index, columns });
+    });
   }
 };
 
