a8ac48e200d68d89993f523c557367b8929346f2
Nathan Winch <nwinch@gmail.com>
2016-05-03 16:52:05 +1000

    Remove data mutation

--- src/components/table_column.js
+++ src/components/table_column.js
@@ -1,20 +1,5 @@
-import { isString } from 'lodash';
-import React, { createFactory, DOM } from 'react';
-import action from './action';
-
-const Action = createFactory(action);
+import React, { DOM } from 'react';
 
 export default function TableColumn({ key = 0, children }) {
-
-  // TODO: This is a flimsy way of checking for a link.  Make this more
-  // robust.
-  // @nwinch 08/04/16
-  if (isString(children) && children.match(/^\//)) {
-    children = Action({
-      className: 'red-text text-accent-4',
-      path: `/collections${children}`,
-    }, children);
-  }
-
   return DOM.td({ key, className: 'grey-text text-darken-2' }, children);
 };
